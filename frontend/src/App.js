2274d4775363a99d83f05dfeb45082f1858369ae
Aalap Patel <aalappatel07@gmail.com>
2020-10-27 22:44:55 -0500

    implement history for sent messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import React, { Component } from "react";
 import { connect, sendMsg } from "./api";
 import Header from './components/Header/Header';
+import History from './components/History/History';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    connect();
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount() {
+    connect((msg)=>{
+      console.log("New message");
+      this.setState(prevState=>({chatHistory:[...this.state.chatHistory,msg]
+      }))
+      console.log(this.state);
+    });
   }
 
   send() {
@@ -18,6 +30,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
+        <History chatHistory={this.state.chatHistory}/>
         <button onClick={this.send}>Send message</button>
       </div>
     );
